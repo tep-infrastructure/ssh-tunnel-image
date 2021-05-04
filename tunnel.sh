d67a6a2215432ef14c5aed0b118433229d954434
Thomas Holmes <thomasholmes@outlook.com>
2021-05-04 20:50:43 +0100

    PEM is now copied to workaround readonly file systems.

--- tunnel.sh
+++ tunnel.sh
@@ -6,9 +6,10 @@ echo "Out port: $OUT_PORT"
 echo "Target: $TARGET"
 echo "PEM path: $PEM_PATH"
 
-chmod 400 $PEM_PATH
+cp $PEM_PATH tunnel.pem
+chmod 400 tunnel.pem
 
 ssh -vvv -o StrictHostKeyChecking=no \
     -o ServerAliveInterval=60 \
     -o ExitOnForwardFailure=yes \
-    -N -R $IN_PORT:$OUT_SERVER:$OUT_PORT -i $PEM_PATH $TARGET
+    -N -R $IN_PORT:$OUT_SERVER:$OUT_PORT -i tunnel.pem $TARGET
